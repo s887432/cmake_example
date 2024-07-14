a25ccdfbb4d5f9ff62debf5c2eec7dfcfb863451
Patrick Lin <patrick.cclin@gmail.com>
2024-07-14 10:57:38 +0800

    Add files via upload

---
+++ src/func.h
@@ -0,0 +1,9 @@
+#ifndef __FUNC_H__
+#define __FUNC_H__
+
+int func(void);
+
+#endif // end of __FUNC_H__
+
+// end of file
+
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "func.h"
+
+int main(int argc, char **argv)
+{
+	printf("This is a cmake project demo\r\n");
+	printf("%d\r\n", func());
+	
+	return 0;
+}
+
+// end of file
+
